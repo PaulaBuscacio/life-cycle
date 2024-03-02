23f63fb814de5c5cc7d39de7d1c41544e57d1ff6
PaulaBuscacio <66645636+PaulaBuscacio@users.noreply.github.com>
2024-03-02 11:15:12 -0300

    pull request developer into main (#1)
    * criacao e edicao do item da lista
    
    * salvar o item como comprado e mudança de estilo quando checked
    
    * delecao item e lista
    
    * refatoracao e edicao do README
    
    * limpeza de local storage e adicao de itens na lista limpa

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, DoCheck, OnInit } from '@angular/core';
+import { Item } from './interfaces/iItem';
+import { ListaDeCompraService } from './service/lista-de-compra.service';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, DoCheck{
   title = 'app-lista-de-compras';
+  listaDeCompra! : Array<Item>
+  itemParaSerEditado! : Item
+  unsubscribe : Subject<void> = new Subject<void>
 
-  constructor() { }
+  constructor(private listaService : ListaDeCompraService) { }
+
+  ngOnInit(): void {
+    this.listaDeCompra = this.listaService.getListaDeCompra()
+    this.listaService.atualizarLocalStorage()
+  }
+
+  ngDoCheck() {
+  //  this.listaService.atualizarLocalStorage()  //se deixar nao apaga o local srtorage por isso foi para o onInit
+  }
+
+  editarItem(item : Item){
+    this.itemParaSerEditado = item
+  }
+
+  deletarItem(id : Number) {
+    const index = this.listaDeCompra.findIndex(item => item.id === id)
+    this.listaDeCompra.splice(index, 1)
+  }
+
+  limparLista(){
+   this.listaService.limparLocalStorage()
+   this.listaDeCompra = []
+   window.location.reload()
+
+
+  }
+
 }
--- src/app/components/item/item.component.ts
+++ src/app/components/item/item.component.ts
@@ -1,18 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, OnDestroy, OnInit, Output, SimpleChanges } from '@angular/core';
 import { faPen, faTrash } from '@fortawesome/free-solid-svg-icons';
+import { Item } from 'src/app/interfaces/iItem';
+import { ListaDeCompraService } from 'src/app/service/lista-de-compra.service';
 
 @Component({
   selector: 'app-item',
   templateUrl: './item.component.html',
   styleUrls: ['./item.component.css']
 })
-export class ItemComponent implements OnInit {
-
-  faPen = faPen;
+export class ItemComponent implements OnInit, OnChanges, OnDestroy {
+  @Input() item!: Item
+  @Output() emitindoItemParaEditar =  new EventEmitter()
+  @Output() emitindoItemParaDeletar = new EventEmitter()
+  faPen = faPen
   faTrash = faTrash
 
-  constructor() { }
+  constructor(private listaDeComprasService : ListaDeCompraService) { }
 
-  ngOnInit(): void { }
+  ngOnInit(): void {}
+
+  ngOnChanges() {}
+
+  editarItem(){
+    this.emitindoItemParaEditar.emit(this.item)
+
+  }
+
+  marcarComprado(){
+    this.listaDeComprasService.mudarComprado(this.item)
+
+  }
+
+  deletarItem() {
+    this.emitindoItemParaDeletar.emit(this.item.id)
+
+  }
+
+  ngOnDestroy(){
+    console.log('item deletado ')
+  }
+
 
 }
